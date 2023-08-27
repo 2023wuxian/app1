52bd5081d4b25599000941f980b889525a4c5c3e
2023wuxian <4333330@gmail.com>
2023-08-27 13:59:30 +0800

    test

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -120,7 +120,7 @@ export function SideBar(props: { className?: string }) {
       <div className={styles["sidebar-header"]}>
         <div className={styles["sidebar-title"]}>ChatGpt</div>
         <div className={styles["sidebar-sub-title"]}>
-          添加微信h285426134 提供更多技术支持
+          添加微信【cq-123-456-789】 提供更多技术支持
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
@@ -156,55 +156,55 @@ export function SideBar(props: { className?: string }) {
         </a>
       </div>
 
-      <div className={styles["sidebar-bar-button"]}>
-        <a href="https://test-cr534q4ghsv6.feishu.cn/wiki/W5pdwY3f9iiaPFkqvsYchROOnLc" target="_blank">
-          <IconButton
-            icon={<GithubIcon />}
-            text='AI知识库访问(持续更新）'
-            shadow
-          />
-        </a>
-      </div>
+//       <div className={styles["sidebar-bar-button"]}>
+//         <a href="https://test-cr534q4ghsv6.feishu.cn/wiki/W5pdwY3f9iiaPFkqvsYchROOnLc" target="_blank">
+//           <IconButton
+//             icon={<GithubIcon />}
+//             text='AI知识库访问(持续更新）'
+//             shadow
+//           />
+//         </a>
+//       </div>
 
-      <div className={styles["sidebar-bar-button"]}>
-        <a href="https://test-cr534q4ghsv6.feishu.cn/wiki/P2F5w3As6igoXykawiRcmdhKnkb" target="_blank">
-          <IconButton
-            icon={<GithubIcon />}
-            text='AI工具保姆级指导书(持续更新）'
-            shadow
-          />
-        </a>
-      </div>
+//       <div className={styles["sidebar-bar-button"]}>
+//         <a href="https://test-cr534q4ghsv6.feishu.cn/wiki/P2F5w3As6igoXykawiRcmdhKnkb" target="_blank">
+//           <IconButton
+//             icon={<GithubIcon />}
+//             text='AI工具保姆级指导书(持续更新）'
+//             shadow
+//           />
+//         </a>
+//       </div>
 
-      <div className={styles["sidebar-bar-button"]}>
-        <a href="https://prm.chatgptvip.info/" target="_blank">
-          <IconButton
-            icon={<GithubIcon />}
-            text='ChatGPT 提示词工具'
-            shadow
-          />
-        </a>
-      </div>
+//       <div className={styles["sidebar-bar-button"]}>
+//         <a href="https://prm.chatgptvip.info/" target="_blank">
+//           <IconButton
+//             icon={<GithubIcon />}
+//             text='ChatGPT 提示词工具'
+//             shadow
+//           />
+//         </a>
+//       </div>
 
-      <div className={styles["sidebar-bar-button"]}>
-        <a href="https://test-cr534q4ghsv6.feishu.cn/wiki/Cq8ywC9t6ib59SkLYkjcUNw9ntd" target="_blank">
-          <IconButton
-            icon={<GithubIcon />}
-            text='企业独立ChatGpt站点'
-            shadow
-          />
-        </a>
-      </div>
+//       <div className={styles["sidebar-bar-button"]}>
+//         <a href="https://test-cr534q4ghsv6.feishu.cn/wiki/Cq8ywC9t6ib59SkLYkjcUNw9ntd" target="_blank">
+//           <IconButton
+//             icon={<GithubIcon />}
+//             text='企业独立ChatGpt站点'
+//             shadow
+//           />
+//         </a>
+//       </div>
 
-      <div className={styles["sidebar-bar-button"]}>
-        <a href="https://test-cr534q4ghsv6.feishu.cn/wiki/SYVUwUhAViLxFRkuSQKcvQx9nQe" target="_blank">
-          <IconButton
-            icon={<GithubIcon />}
-            text='500元搭建企业官网或外贸独立站'
-            shadow
-          />
-        </a>
-      </div>
+//       <div className={styles["sidebar-bar-button"]}>
+//         <a href="https://test-cr534q4ghsv6.feishu.cn/wiki/SYVUwUhAViLxFRkuSQKcvQx9nQe" target="_blank">
+//           <IconButton
+//             icon={<GithubIcon />}
+//             text='500元搭建企业官网或外贸独立站'
+//             shadow
+//           />
+//         </a>
+//       </div>
 
       <div
         className={styles["sidebar-body"]}
